0833252dcaf93e9caad33843ab8831eb19adbbc7
detrasmental <detra.gordon76@gmail.com>
2021-12-21 11:37:49 -0500

    attempt 546485

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
-const path = require('path');
-require('dotenv').config();
 const express = require('express');
-const routes = require('./controllers/');
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
-const exphbs = require('express-handlebars')
-const session = require('express-session')
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const path = require('path');
+
 const helpers = require('./utils/helpers');
 
+const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
-
-const sess = {
-  secret: process.env.DB_SESSION_SECRET,
-  cookie: { maxAge: 7600000 },
-  resave: false,
-  saveUninitialized: true,
-  store: new SequelizeStore({
-    db: sequelize
-  })
-};
+const session = require('express-session');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'supersecretsecretcode',
+  cookie: {
+        // Session will automatically expire in 10 minutes
+        expires: 10 * 60 * 1000
+  },
+  resave: true,
+  rolling: true,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  }),
+};
 
 app.use(session(sess));
 
@@ -35,6 +40,7 @@ app.set('view engine', 'handlebars');
 
 app.use(routes);
 
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
